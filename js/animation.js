81839183f044c1598e06f44963993e9385235d00
Radmir Musabaev <musabaevradmir@gmail.com>
2024-03-14 21:46:33 +0400

    init

--- js/animation.js
+++
@@ -1,7 +0,0 @@
-const animation = lottie.loadAnimation({
-    container: document.getElementById('wallet'), // контейнер для анимации
-    renderer: 'svg', // тип рендерера (может быть 'svg', 'canvas' или 'html')
-    loop: true, // зацикливание анимации
-    autoplay: true, // автоматический запуск анимации
-    path: 'animations/wallet.json' // путь к вашему JSON-файлу с анимацией
-});
