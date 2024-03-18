e88f0c343f51d84eccb6d2856482b94de935a65a
Алиев Ислам <salamvsem4567@gmail.com>
2024-03-18 18:27:21 +0300

    update showcases-slider

---
+++ js/showcase-slider.js
@@ -0,0 +1,35 @@
+function initSlider() {
+  const slider = document
+    .getElementById("showcases-slider")
+    .querySelector(".showcases__slider");
+  const nextButton = document
+    .getElementById("showcases-slider")
+    .querySelector(".showcases__arrow--right");
+  const prevButton = document
+    .getElementById("showcases-slider")
+    .querySelector(".showcases__arrow--left");
+  let slides = Array.from(slider.children); // Собираем массив слайдов
+  let currentIndex = 0;
+  // Скрытие стрелки назад в начале
+  nextButton.addEventListener("click", () => {
+    currentIndex++;
+    slider.scrollTo({
+      left: currentIndex * 300,
+      behavior: "smooth",
+    });
+  });
+
+  prevButton.addEventListener("click", () => {
+    currentIndex--;
+    slider.scrollTo({
+      left: currentIndex * 300,
+      behavior: "smooth",
+    });
+  });
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  if (window.innerWidth < 769) {
+    initSlider();
+  }
+});
