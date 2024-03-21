134953a49d780ff5ab684c3e5a7281e29429668a
Radmir Musabaev <musabaevradmir@gmail.com>
2024-03-21 15:06:10 +0400

    final scale

---
+++ js/adaptive-scale.js
@@ -0,0 +1,8 @@
+document.addEventListener("resize", function () {
+    if (window.devicePixelRatio !== 1) { // Костыль для определения иных устройств, с коэффициентом отличным от 1
+        var dpt = window.devicePixelRatio;
+        var widthM = window.screen.width * dpt;
+        var widthH = window.screen.height * dpt;
+        document.write('<meta name="viewport" content="width=' + widthM+ ', height=' + widthH + '">');
+    }
+})
